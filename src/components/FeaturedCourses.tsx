d2e1ee903739ac351580f8e7eceaa09cd64f8eb3
abhishek patel <abhishekpatel6687.me>
2025-01-12 23:30:04 +0530

    whychooseus page

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -38,8 +38,8 @@ const FeaturedCourses = () => {
               <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
                 <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
                   <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
-                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow ">{course.description}</p>
-                  <Link href={`/courses/${course.slug}`}>Learm More</Link>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow mb-4 ">{course.description}</p>
+                  <Link href={`/courses/${course.slug}`}>Learn More</Link>
                 </div>
               </BackgroundGradient>
             </div>

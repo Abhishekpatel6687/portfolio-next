e052c835ec7e42d3de913a5d175fc6f7653a00d8
abhishek patel <abhishekpatel6687.me>
2025-01-17 00:28:28 +0530

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,23 @@
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <div className="max-w-full py-8 px-12 bg-red-400 grid gap-28 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-center ">
+      {/* <div className="w-auto bg-pink-950"> */}
+      <div className=" bg-green-600 px-2">
+        <h2 className="text-center py-2"> This is Footer</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laudantium cum accusamus! Harum, quod! Ipsa fuga porro nisi non aut esse. Temporibus, voluptatem odit doloremque error quas obcaecati accusamus deleniti.</p>
+      </div>
+      <div className=" bg-black px-2">
+        <h2 className="text-center py-2"> This is Footer</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laudantium cum accusamus! Harum, quod! Ipsa fuga porro nisi non aut esse. Temporibus, voluptatem odit doloremque error quas obcaecati accusamus deleniti.</p>
+      </div>
+      <div className=" bg-blue-600 px-2">
+        <h2 className="text-center py-2"> This is Footer</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laudantium cum accusamus! Harum, quod! Ipsa fuga porro nisi non aut esse. Temporibus, voluptatem odit doloremque error quas obcaecati accusamus deleniti.</p>
+      </div>
+      {/* </div> */}
+      
+    </div>
   )
 }
 

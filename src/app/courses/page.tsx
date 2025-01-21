d5d6951efa9d2ff20ab50d9a744cf4eb39ec902a
abhishekpatel6687 <abhishekpatel6687@gmail.com>
2025-01-21 23:58:03 +0530

    all courses 3d card

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "../../components/ui/3d-card";
+import Link from "next/link";
+
+ function page() {
+  return (
+    <div>
+        <h1>All courses</h1>
+    </div>
+  );
+}
+export default page

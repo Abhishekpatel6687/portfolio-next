0f66f1b2b14efc84b6df87f2cb05f480f7a42fee
abhishek patel <abhishekpatel6687.me>
2025-02-11 00:09:24 +0530

    d

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,31 +4,6 @@ import { Spotlight } from "./ui/Spotlight";
 
 const HeroSection = () => {
   return (
-    // <div className="h-auto md:h-[40rem]  w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-    //   <div className="p-4 relative z-10 w-full text-center">
-    //     <Spotlight
-    //       className="-top-40 left-0 md:left-60 md:-top-20"
-    //       fill="white"
-    //     />
-    //     <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-    //       Master the Art of Music
-    //     </h1>
-    //     <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-    //       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-    //       aliquam. Numquam, deleniti? Commodi, sed quasi quisquam.
-    //     </p>
-    //     <div className="mt-4">
-    //       <Link href={"/courses"}>
-    //         <Button
-    //           borderRadius="1.75rem"
-    //           className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-    //         >
-    //           Explore courses
-    //         </Button>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="min-h-screen w-full bg-green-100 flex text-black">
     {/* Spotlight component */}
     <Spotlight

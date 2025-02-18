9b6dff3fb94d93e46e68cb2a3faffb243782e3bd
abhishek patel <abhishekpatel6687.me>
2025-02-18 23:55:08 +0530

    home

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import courseData from "../data/music_courses.json";
 import { BackgroundGradient } from "./ui/background-gradient";
 
-interface Course {
+interface Course { 
   id: number;
   title: string;
   slug: string;
@@ -12,7 +12,7 @@ interface Course {
   instructor: string;
   category: string;
   isFeatured: boolean;
-  // image: "/courses/guitar.jpg"
+  image: string;
 }
 const About = () => {
   const featuredCourses = courseData.courses.filter(
@@ -33,12 +33,13 @@ const About = () => {
         </div>
       </div>
       <div className="mt-10 ">
-        <div className=" grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-5 gap-2 justify-center">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xxl:grid-cols-8 px-20  gap-8 justify-center">
           {featuredCourses.map((course: Course) => (
             <div key={course.id} className="flex justify-center">
-              <BackgroundGradient className="flex flex-col rounded-[22px]  dark:bg-white overflow-hidden h-full max-w-60">
-                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
-                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
+              <BackgroundGradient className="flex flex-col rounded-[22px]  dark:bg-white overflow-hidden h-full max-w-72">
+                <div className="p-4 sm:p-6 flex flex-col md:flex-4  items-center text-center flex-grow">
+                  <img src={course.image} alt="img" className="w-24 h-24 object-cover" />
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 font-bold">{course.title}</p>
                   <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow mb-4 ">{course.description}</p>
                   <Link href={`/courses/${course.slug}`}>Learn More</Link>
                 </div>
@@ -47,7 +48,7 @@ const About = () => {
           ))}
         </div>
       </div>
-      <div className="mt-20 text-center">
+      {/* <div className="mt-20 text-center">
         <Link
           href={"/courses"}
           className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100
@@ -55,7 +56,7 @@ const About = () => {
         >
           View all courses
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 };

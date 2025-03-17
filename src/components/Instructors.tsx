15a2bf9895b103749324be44c66a64c43ef1a683
abhishek patel <abhishekpatel6687.me>
2025-03-17 23:58:44 +0530

    aa

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -45,7 +45,7 @@ const instructors = () => {
   return (
     <div className="relative h-[18rem] overflow-hidden flex justify-center">
       <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col  justify-center">
-        <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">
+        <h2 className="text-2xl md:text-4xl lg:text-6xl text-white font-bold text-center mb-6">
           Frontend Developer
         </h2>
         <p className="text-base md:text-lg text-white text-center mb-4">

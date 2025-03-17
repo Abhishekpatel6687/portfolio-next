fa8483d1fdcdf3dfb98329e26b4e78e9f1dbec6e
abhishek patel <abhishekpatel6687.me>
2025-03-17 23:58:26 +0530

    multi link

--- src/components/Multimedia.tsx
+++ src/components/Multimedia.tsx
@@ -1,37 +1,43 @@
-import React from 'react'
+import React from "react";
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 const Multimedia = () => {
   const people = [
     {
-      id: 1, 
+      id: 1,
       name: "LinkedIn",
+      link: "https://www.linkedin.com/in/abhishek-patel-2a6599240/",
       designation: "Software Engineer",
-      image:"mage:linkedin",
+      image: "mage:linkedin",
     },
     {
       id: 2,
       name: "Github",
+      link: "https://github.com/Abhishekpatel6687",
       designation: "UI/UX Designer",
-      image:"akar-icons:github-fill",
+      image: "akar-icons:github-fill",
     },
     {
       id: 3,
       name: "Instagram",
+      link: "https://www.instagram.com/simple_boy_abhishek_6687/",
       designation: "Product Manager",
-      image:"lineicons:instagram",
+      image: "lineicons:instagram",
     },
     {
       id: 4,
       name: "WhatsApp",
+      link: "https://wa.me/917071326687",
       designation: "Data Scientist",
-      image:"ph:whatsapp-logo-light",
+      image: "ph:whatsapp-logo-light",
     },
     {
       id: 5,
       name: "Facebook",
+      link: "https://www.facebook.com/share/18UwsXTjdT/",
       designation: "Marketing Specialist",
-      image:"formkit:facebook",
+      image: "formkit:facebook",
     },
     // {
     //   id: 6,
@@ -42,24 +48,26 @@ const Multimedia = () => {
   ];
   return (
     <div className="flex gap-1 mx-2 flex-col text-black">
-    {people.map((item, i) => (
-      <div
-        key={i}
-        className="h-10 w-10 flex items-center justify-center bg-white hover:bg-gray-400 rounded-full 
+      {people.map((item, i) => (
+        <div
+          key={i}
+          className="h-10 w-10 flex items-center justify-center bg-white hover:bg-gray-400 rounded-full 
                    border border-gray-300 shadow-md transition-all duration-300 
                    hover:scale-110 hover:shadow-lg hover:border-white"
-      >
-        <Icon
-          width={28}
-          icon={item.image}
-          className="rounded-full cursor-pointer text-gray-500 bg-white p-1 
-                     transition-all duration-300 ease-in-out 
-                     hover:text-white hover:bg-gray-400"
-        />
-      </div>
-    ))}
-  </div>
-  )
-}
+        >
+          <Link href={item.link || "/"}>
+            <Icon
+              width={28}
+              icon={item.image}
+              className="rounded-full cursor-pointer text-gray-500 bg-white p-1 
+                   transition-all duration-300 ease-in-out 
+                   hover:text-white hover:bg-gray-400"
+            />
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Multimedia
+export default Multimedia;

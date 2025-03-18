17b755a1f3af52055dc2371acc2bf0fbe0a68215
abhishek patel <abhishekpatel6687.me>
2025-03-18 22:44:35 +0530

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,34 @@ const page = () => {
   return (
     // bg-grid-white/[0.02]
    <main className='min-h-screen bg-backColor antialiased '>
-<HeroSection/>
-<About/>
-<UpcommingWebinars/>
-<SkillsEducation/>
-{/* <WhyChooseUS/> */}
-<MovingCard/>
-<Contact />
-<Instructors/>
-{/* <Footer/> */}
+    <div id="/">
+        <HeroSection />
+      </div>
+      <div id="about">
+        <About />
+      </div>
+      <div id="upcommingWebinars">
+        <UpcommingWebinars />
+      </div>
+      <div id="skillsEducation">
+        <SkillsEducation />
+      </div>
+      <div id="whyChooseUS">
+        <WhyChooseUS />
+      </div>
+      <div id="movingCard">
+        <MovingCard />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+      <div id="instructors">
+        <Instructors />
+      </div>
+      <div id="footer">
+        <Footer />
+      </div>
+
    </main>
   )
 }

eeb3e960aa766056cfdb704ae91e1a0f488f4894
abhishek patel <abhishekpatel6687.me>
2025-04-09 23:07:46 +0530

    home

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,11 +15,10 @@ const textColor = [
 ];
 
 const HeroSection = () => {
-  const profession = useMemo(() => [
-    "I am React Developer",
-    "I am Designer",
-    "I am Photographer",
-  ], []);
+  const profession = useMemo(
+    () => ["I am React Developer", "I am Designer", "I am Photographer"],
+    []
+  );
   const [index, setIndex] = useState(0);
   const [displayText, setDisplayText] = useState("");
   const [charIndex, setCharIndex] = useState(0);
@@ -77,8 +76,13 @@ const HeroSection = () => {
           <div className="w-4 h-4 bg-gradient-to-r from-green-100 to-white rounded-full mr-1 shadow-md" />
           <SlideIn direction="right">
             <h1 className="text-xl lg:text-3xl font-bold text-black">Hello,</h1>
+          </SlideIn>
+
+          <SlideIn direction="right">
             <h1 className="xs:mt-5 lg:mt-7 text-4xl lg:text-5xl font-bold text-green-800">
               I am Abhishek <br />
+
+
               <span className="text-2xl relative top-[-10px]">
                 <div
                   className={`${currentColor} text-2xl font-semibold text-start mt-6 h-10`}
@@ -99,51 +103,49 @@ const HeroSection = () => {
       <div className="relative w-full md:w-[56%] flex justify-center md:block">
         <div className="w-20 h-20 md:w-64 md:h-64 lg:w-80 lg:h-80 bg-gradient-to-r from-green-100 to-white rounded-full ml-[26rem] md:ml-[26rem] lg:ml-[44rem] shadow-2xl absolute" />
         <SlideIn direction="left" delay={0.2}>
-  <motion.div
-    animate={{ y: [0, -10, 0] }}
-    transition={{
-      duration: 1.5,
-      ease: "easeInOut",
-      repeat: Infinity,
-    }}
-  >
-    <div className="w-16 h-16 md:w-24 md:h-24 lg:w-28 lg:h-28 bg-white rounded-full flex justify-center items-center shadow-2xl absolute ml-[-14.2rem] sm:ml-[-20rem] md:ml-[18rem] lg:ml-[24rem] mt-5 md:mt-[5rem]">
-      <Image
-        src="/logoImage/react.png"
-        alt="React Logo"
-        width={48}
-        height={48}
-        className="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20"
-      />
-    </div>
-  </motion.div>
-</SlideIn>
+          <motion.div
+            animate={{ y: [0, -10, 0] }}
+            transition={{
+              duration: 1.5,
+              ease: "easeInOut",
+              repeat: Infinity,
+            }}
+          >
+            <div className="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 bg-white rounded-full flex justify-center items-center shadow-2xl absolute ml-[-8rem] sm:ml-[-10rem] md:ml-[17rem] lg:ml-[24rem] mt-5 md:mt-[5rem]">
+              <Image
+                src="/logoImage/react.png"
+                alt="React Logo"
+                width={48}
+                height={48}
+                className="w-12 h-12 md:w-14 md:h-14 lg:w-16 lg:h-16  "
+              />
+            </div>
+          </motion.div>
+        </SlideIn>
 
-
-<SlideIn direction="left" delay={0.4}>
-  <motion.div
-    animate={{ y: [0, -10, 0] }}
-    transition={{
-      duration: 1.5,
-      ease: "easeInOut",
-      repeat: Infinity,
-    }}
-  >
-    <div className="w-16 h-16 md:w-24 md:h-24 lg:w-28 lg:h-28 bg-white rounded-full shadow-2xl absolute ml-[-17.4rem] sm:ml-[-22rem] md:ml-[20.6rem] lg:ml-[28rem] mt-28 md:mt-[13rem] lg:mt-[16rem]">
-      <Image
-        src="/logoImage/js.png"
-        alt="JavaScript Logo"
-        width={56}
-        height={56}
-        className="w-14 h-14 md:w-24 md:h-24 lg:w-24 lg:h-24 md:pr-3 lg:pr-0 md:mt-[-0.8rem] lg:mt-[-0.6rem]"
-      />
-    </div>
-  </motion.div>
-</SlideIn>
-
+        <SlideIn direction="left" delay={0.4}>
+          <motion.div
+            animate={{ y: [0, -10, 0] }}
+            transition={{
+              duration: 1.5,
+              ease: "easeInOut",
+              repeat: Infinity,
+            }}
+          >
+            <div className="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 bg-white rounded-full shadow-2xl absolute ml-[-10rem] sm:ml-[-12rem] md:ml-[19.6rem] lg:ml-[28rem] mt-28 md:mt-[13rem] lg:mt-[16rem]">
+              <Image
+                src="/logoImage/js.png"
+                alt="JavaScript Logo"
+                width={56}
+                height={56}
+                className="w-14 h-14 md:w-24 md:h-20 lg:w-16 lg:h-20 md:pr-3 lg:pr-0 -mt-1 md:mt-[-0.8rem] lg:ml-2 lg:mt-[-0.4rem]"
+              />
+            </div>
+          </motion.div>
+        </SlideIn>
 
         <SlideIn direction="inner">
-          <div className="origin-center w-60 h-60 md:w-72 md:h-72 lg:w-96 lg:h-96 bg-gradient-to-r from-green-100 to-white rounded-full shadow-2xl mt-[1rem] ml-20 sm:ml-0 md:mt-28 absolute md:relative">
+          <div className="origin-center w-60 h-60 md:w-72 md:h-72 lg:w-96 lg:h-96 bg-gradient-to-r from-green-100 to-white rounded-full shadow-2xl mt-[1rem]  -ml-20 md:ml-0 md:mt-28  absolute md:relative">
             <Image
               src="/Image/my1.png"
               alt="My Image"
@@ -155,22 +157,23 @@ const HeroSection = () => {
         </SlideIn>
 
         <SlideIn direction="left">
-        <motion.div
-    animate={{ y: [0, -10, 0] }}
-    transition={{
-      duration: 1.5,
-      ease: "easeInOut",
-      repeat: Infinity,
-    }}>
-          <div className="w-16 h-16 md:w-24 md:h-24 lg:w-28 lg:h-28 bg-white rounded-full flex justify-center items-center shadow-2xl absolute ml-[-13.4rem] sm:ml-[-19rem] md:ml-[18rem] lg:ml-[23.8rem] mt-52 md:mt-[-2.4rem] lg:mt-[-3.8rem]">
-            <Image
-              src="/logoImage/redux.png"
-              alt="Redux Logo"
-              width={80}
-              height={80}
-              className="w-10 h-10 md:w-16 md:h-16 lg:w-20 lg:h-20"
-            />
-          </div>
+          <motion.div
+            animate={{ y: [0, -10, 0] }}
+            transition={{
+              duration: 1.5,
+              ease: "easeInOut",
+              repeat: Infinity,
+            }}
+          >
+            <div className="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 bg-white rounded-full flex justify-center items-center shadow-2xl absolute ml-[-8rem] sm:ml-[-10rem] md:ml-[18rem] lg:ml-[23.8rem] mt-52 md:mt-[-2.4rem] lg:mt-[-3.8rem]">
+              <Image
+                src="/logoImage/redux.png"
+                alt="Redux Logo"
+                width={80}
+                height={80}
+                className="w-10 h-10 md:w-12 md:h-12 lg:w-16 lg:h-16  "
+              />
+            </div>
           </motion.div>
         </SlideIn>
 
